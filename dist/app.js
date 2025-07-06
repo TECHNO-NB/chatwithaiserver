56bf1a0e3f8bd31d5ca066a65c6ba452202f20d7
NareshBhattarai4 <technologyandapplication@gmail.com>
2025-07-06 21:45:14 +0545

    add trust proxy

--- dist/app.js
+++ dist/app.js
@@ -13,20 +13,24 @@ import rateLimit from "express-rate-limit";
 const app = express();
 // error handler
 errorHandler();
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTED_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
-}));
+  })
+);
+app.set("trust proxy", 1);
+
 const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    max: 100,
-    standardHeaders: true,
-    legacyHeaders: false,
-    message: {
-        status: 429,
-        error: "Too many requests, please try again later.",
-    },
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    status: 429,
+    error: "Too many requests, please try again later.",
+  },
 });
 app.use(limiter);
 app.use(cookieParser());
@@ -41,7 +45,7 @@ app.use("/api/v1/users", usersRoute);
 app.use("/api/v1/doc", docRoute);
 app.use("/api/v1/payment", checkOut);
 app.post("/", (req, res) => {
-    res.send("Server is working");
-    console.log("server is working");
+  res.send("Server is working");
+  console.log("server is working");
 });
 export default app;
